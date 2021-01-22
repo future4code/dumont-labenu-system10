945004ed116d8f7aa2019e35ecdd6c2121741af6
AlvaroBarrosJunior <alvarojoaquimjr@gmail.com>
2021-01-22 10:43:30 -0300

    Resolvi um erro que havia no código anteriormente

--- template-express-knex/src/data/selectStudentsSameHobby.ts
+++ template-express-knex/src/data/selectStudentsSameHobby.ts
@@ -4,7 +4,7 @@ export const selectStudentsSameHobby = async (id: number): Promise<any> => {
   const result = await connection("student")
     .join("student_hobby", "student.id", "=", "student_hobby.student_id")
     .join("hobby", "student_hobby.hobby_id", "=", "hobby.id")
-    .select("student.name", "student.email", "student.birthdate")
+    .select("student.name", "student.email", "student.birthdate", "hobby.name as hobby_Name")
     .where("hobby.id", id)
 
   return result
